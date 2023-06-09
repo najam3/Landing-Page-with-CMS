c06c14e2f751f23a3c4c486473ef6de7ab400992
najam3 <100590374+najam3@users.noreply.github.com>
2023-06-10 03:44:43 +0500

    added cms

--- .eleventy.js
+++ .eleventy.js
@@ -5,8 +5,9 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/new.js');
     eleventyConfig.addPassthroughCopy('/src/images');
     eleventyConfig.addPassthroughCopy('/src/inventory');
-    eleventyConfig.addPassthroughCopy('/src/admin');
-    
+    eleventyConfig.addPassthroughCopy('/src/admin/config.yml');
+    eleventyConfig.addPassthroughCopy('/src/admin/index.html');
+        
     return {
         dir: {
             input: "src",
