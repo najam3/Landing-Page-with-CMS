3e54bf5478e52d3a8428e54061d9367698b5ea1f
najam3 <100590374+najam3@users.noreply.github.com>
2023-06-10 06:08:46 +0500

    config file updated

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,11 @@
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy('./src/index.css');
-    eleventyConfig.addPassthroughCopy('./src/script.js');
-    eleventyConfig.addPassthroughCopy('./src/new.js');
-    eleventyConfig.addPassthroughCopy('./src/images/homes');
-    eleventyConfig.addPassthroughCopy('./src/images');
-    eleventyConfig.addPassthroughCopy('./src/inventory');
-    eleventyConfig.addPassthroughCopy('./src/admin/config.yml');
-    eleventyConfig.addPassthroughCopy('./src/admin/index.html');
+    eleventyConfig.addPassthroughCopy('/src/index.css');
+    eleventyConfig.addPassthroughCopy('/src/images');
+    eleventyConfig.addPassthroughCopy('/src/icons');
+    eleventyConfig.addPassthroughCopy('/src/inventory');
+    eleventyConfig.addPassthroughCopy('/src/admin');
+
         
     return {
         dir: {
