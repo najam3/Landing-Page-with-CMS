58f274cccd7fa3904ad44911bf08747d0d222209
najam3 <100590374+najam3@users.noreply.github.com>
2023-06-21 01:41:30 +0500

    Initial Commit

--- src/script.js
+++ src/script.js
@@ -1,13 +1,3 @@
 const card =  document.querySelectorAll('.card')
 const button = document.querySelectorAll('.my-btn')
 const expolore = document.getElementById('explore');
-
-let x;
-button.forEach(btn => {
-    btn.addEventListener('click', e => {
-        x = e.target.dataset.index
-        localStorage.setItem('uniqueId', x)
-    })
-})
-
-
